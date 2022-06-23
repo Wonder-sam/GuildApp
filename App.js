9a9d55306ef82d2336010de6861f8bc695710598
Wonder <samuel.nai@yahoo.com>
2022-06-23 02:09:20 +0000

    create navigation stack

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Welcome from './Screens/Welcome.js'
+import Login from './Screens/Login.js'
+import BottomTabs from './Screens/BottomTabs.js'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	return (
+	<NavigationContainer>
+		<Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
+			<Stack.Screen name="Welcome" component={Welcome} />
+			<Stack.Screen name="Login" component={Login} />
+			<Stack.Screen name= "BottomTabs" component={BottomTabs} />
+		</Stack.Navigator>
+	</NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
